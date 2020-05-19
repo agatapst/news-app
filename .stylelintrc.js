bc6ecf8ba75579791cf4f95375c34806ae9256bb
Agata Stawarz <agata.stawarz94@gmail.com>
2020-05-19 18:52:22 +0200

    Set up linters

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-prettier',
+    'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': true,
+  },
+};
