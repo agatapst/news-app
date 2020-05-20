a4e9dd7485b8138e714bb86a6948bc5212e0e602
Agata Stawarz <agata.stawarz94@gmail.com>
2020-05-20 14:04:05 +0200

    Set up testing library

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders news app header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = getByText(/news app/i);
+  expect(element).toBeInTheDocument();
 });
